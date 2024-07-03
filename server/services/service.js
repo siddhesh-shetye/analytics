aa4c200f725d407c8fcff2a5f7e86eed632998ee
siddhesh-shetye <36694851+siddhesh-shetye@users.noreply.github.com>
2024-07-03 15:58:08 +0530

    identifier added create api

--- server/services/service.js
+++ server/services/service.js
@@ -31,7 +31,7 @@ module.exports = ({ strapi }) => ({
   },
 
   async create(ctx) {
-    const { event_type, collection_name, collection_id, field } = ctx.request.body;
+    const { event_type, collection_name, collection_id, field, identifier } = ctx.request.body;
     const { user } = ctx.state;
 
 
@@ -48,6 +48,7 @@ module.exports = ({ strapi }) => ({
       collection_name,
       collection_id,
       field,
+      identifier,
       user_token: user.id ? user.id.toString() : (user.guest_id || ''),
       publishedAt: new Date()
     };
